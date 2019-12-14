d1bf79b8de0e7ad2f7bddb4c6b725451e0ab56a9
Cheulsoon Baek <qorcjftns2@gmail.com>
2019-12-13 22:30:01 -0500

    folder sturcure changed / basic index html added

---
+++ src/pages/intro/index.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+
+class Intro extends Component {
+
+    render() {
+        return (
+            <div className="test">App</div>
+        );
+    }
+
+}
+
+export default Intro;
